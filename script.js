bfe846c88f701c29f5bd3411e4ed09547d4076ad
sign4511 <sign4511@stud.kea.dk>
2020-11-06 10:14:22 +0100

    begyndt på burger menu i index.html  og script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let filter = "alle";
 let content;
 let container = document.querySelector("#container");
 let temp = document.querySelector("template");
 
+window.addEventListener("load", sidenVises);
 
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector(".menuknap").addEventListener("click", toggleMenu);
+
+}
 
 const link = "http://vais.dk/kea/02_SEM/tema9/ihs/wordpress/wp-json/wp/v2/pages/" + id;
 
@@ -17,4 +23,19 @@ async function hentData() {
     console.log(content.content.rendered);
     document.querySelector("section").innerHTML = content.content.rendered;
 
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+
+    document.querySelector(".menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector(".menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector(".menuknap").innerHTML = "<img src='burgermenu.png' alt='menu logo'>";
+
+    } else {
+        document.querySelector(".menuknap").innerHTML = "<img src='kryds.png' alt='menu x'>";
+    }
 }
